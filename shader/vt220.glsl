9cf99a05b45e5d880bb72d8e756a8c6d4f1ed439
Jason Klein <jklein.64@verizon.net>
2025-11-25 18:57:51 -0500

    feat: make sense of shader a bit

--- shader/vt220.glsl
+++ shader/vt220.glsl
@@ -19,65 +19,32 @@
 
 precision highp float;
 
+in vec2 texcoord;
+out vec4 frag_color;
+
 // using normal vectors of a sphere with radius r
-vec2 crtCurve(vec2 uv, float r) {
+vec2 crt_warp(vec2 uv, float r) {
     uv = (uv - 0.5) * 2.0;// uv is now -1 to 1
     uv = r * uv / sqrt(r * r - dot(uv, uv));
     uv = (uv / 2.0) + 0.5;// back to 0-1 coords
     return uv;
 }
 
-float roundSquare(vec2 p, vec2 b, float r) {
+float squircle(vec2 p, vec2 b, float r) {
     return length(max(abs(p) - b, 0.0)) - r;
 }
 
-float rand(vec2 co) {
-    return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
+float squircle_border(vec2 uv, vec2 dims_inner, vec2 dims_outer) {
+    float dist_inner = squircle(uv - vec2(0.5, 0.5), dims_inner, 0.05);
+    float dist_outer = squircle(uv - vec2(0.5, 0.5), dims_outer, 0.05);
+    return smoothstep(-SMOOTH * 2.0, SMOOTH * 10.0, dist_inner) *
+        smoothstep(SMOOTH * 3.0, -SMOOTH * 2.0, dist_outer);
 }
 
-// Calculate normal to distance function and move along
-// normal with distance to get point of reflection
-//vec2 borderReflect(vec2 p, float r)
-//{
- //   float eps = 0.0001;
- //   vec2 epsx = vec2(eps,0.0);
- //   vec2 epsy = vec2(0.0,eps);
- //   vec2 b = (1.+vec2(r,r))* 0.5;
- //   r /= 3.0;
- //
- //   p -= 0.5;
- //   vec2 normal = vec2(roundSquare(p-epsx,b,r)-roundSquare(p+epsx,b,r),
-  //                     roundSquare(p-epsy,b,r)-roundSquare(p+epsy,b,r))/eps;
-  //  float d = roundSquare(p, b, r);
-  //  p += 0.5;
-   // return p + d*normal;
-//}
-
-// Some Plasma stolen from dogeshibu for testing
-// float somePlasma(vec2 uv)
-// {
-//     if(uv.x < 0.0 || uv.x > 1.0 ||  uv.y < 0.0 || uv.y > 1.0) return 0.0;
-
-//     float scln = 0.5 - 0.5*cos(uv.y*3.14*NO_OF_LINES); // scanlines
-//     uv *= vec2(80, 24); // 80 by 24 characters
-//     uv = ceil(uv);
-//     uv /= vec2(80, 24);
-
-//     float color = 0.0;
-//     color += 0.7*sin(0.5*uv.x + iTime/5.0);
-//     color += 3.0*sin(1.6*uv.y + iTime/5.0);
-//     color += 1.0*sin(10.0*(uv.y * sin(iTime/2.0) + uv.x * cos(iTime/5.0)) + iTime/2.0);
-//     float cx = uv.x + 0.5*sin(iTime/2.0);
-//     float cy = uv.y + 0.5*cos(iTime/4.0);
-//     color += 0.4*sin(sqrt(100.0*cx*cx + 100.0*cy*cy + 1.0) + iTime);
-//     color += 0.9*sin(sqrt(75.0*cx*cx + 25.0*cy*cy + 1.0) + iTime);
-//     color += -1.4*sin(sqrt(256.0*cx*cx + 25.0*cy*cy + 1.0) + iTime);
-//     color += 0.3 * sin(0.5*uv.y + uv.x + sin(iTime));
-//     return scln*floor(3.0*(0.5+0.499*sin(color)))/3.0; // vt220 has 2 intensitiy levels
-// }
-
-in vec2 texcoord;
-out vec4 frag_color;
+// smoothsteps inner from -s.x to s.y and outer from s.
+float squircle_border(vec2 uv, vec2 dims) {
+    return squircle_border(uv, dims, dims);
+}
 
 void main() {
     vec4 c = vec4(0.0, 0.0, 0.0, 0.0);
@@ -88,24 +55,22 @@ void main() {
     float r = CURVE;
 
     // Screen Layer
-    vec2 uvS = crtCurve(uv, r);
+    vec2 uvS = crt_warp(uv, r);
 
     // Enclosure Layer
-    vec2 uvE = crtCurve(uv, r + 0.25);
+    vec2 uvE = crt_warp(uv, r + 0.25);
 
     c += BEZEL_COL * AMBIENT * 0.7 *
-        smoothstep(-SMOOTH, SMOOTH, roundSquare(uvS - vec2(0.5, 0.5), vec2(WIDTH, HEIGHT), 0.05)) *
-        smoothstep(SMOOTH, -SMOOTH, roundSquare(uvE - vec2(0.5, 0.5), vec2(WIDTH, HEIGHT) + 0.05, 0.05));
+        smoothstep(-SMOOTH, SMOOTH, squircle(uvS - vec2(0.5, 0.5), vec2(WIDTH, HEIGHT), 0.05)) *
+        smoothstep(SMOOTH, -SMOOTH, squircle(uvE - vec2(0.5, 0.5), vec2(WIDTH, HEIGHT) + 0.05, 0.05));
 
     // Corner
-    c += (BEZEL_COL) * 0.8 *
-        smoothstep(-SMOOTH * 2.0, SMOOTH * 10.0, roundSquare(uvE - vec2(0.5, 0.5), vec2(WIDTH, HEIGHT) + 0.05, 0.05)) *
-        smoothstep(SMOOTH * 3.0, -SMOOTH * 2.0, roundSquare(uvE - vec2(0.5, 0.5), vec2(WIDTH, HEIGHT) + 0.05, 0.05));
+    c += (BEZEL_COL) * 0.8 * squircle_border(uvE, vec2(WIDTH, HEIGHT) + 0.05);
 
     // Outer Border
     c += BEZEL_COL * AMBIENT *
-        smoothstep(-SMOOTH, SMOOTH, roundSquare(uvE - vec2(0.5, 0.5), vec2(WIDTH, HEIGHT) + 0.05, 0.05)) *
-        smoothstep(SMOOTH, -SMOOTH, roundSquare(uvE - vec2(0.5, 0.5), vec2(WIDTH, HEIGHT) + 0.15, 0.05));
+        smoothstep(-SMOOTH, SMOOTH, squircle(uvE - vec2(0.5, 0.5), vec2(WIDTH, HEIGHT) + 0.05, 0.05)) *
+        smoothstep(SMOOTH, -SMOOTH, squircle(uvE - vec2(0.5, 0.5), vec2(WIDTH, HEIGHT) + 0.15, 0.05));
 
     frag_color = c;
 }
