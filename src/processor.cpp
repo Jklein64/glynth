2c80614ee6574e87c24f53421a37faf75307ad9c
Jason Klein <jklein.64@verizon.net>
2025-11-27 14:15:39 -0500

    feat: add silencer

--- src/processor.cpp
+++ src/processor.cpp
@@ -4,7 +4,25 @@
 #include <fmt/base.h>
 
 //==============================================================================
-GlynthProcessor::GlynthProcessor() : AudioProcessor(s_io_layouts) {}
+GlynthProcessor::GlynthProcessor()
+    : AudioProcessor(s_io_layouts), m_gen(m_rd()), m_dist(-1.f, 1) {
+  m_hpf_freq = new juce::AudioParameterFloat(
+      juce::ParameterID("hpf_freq", 1), "High Cutoff",
+      juce::NormalisableRange(20.0f, 20000.0f), 5.0f);
+  addParameter(m_hpf_freq);
+  m_hpf_res = new juce::AudioParameterFloat(
+      juce::ParameterID("hpf_res", 1), "Resonance",
+      juce::NormalisableRange(0.1f, 100.0f), 0.71f);
+  addParameter(m_hpf_res);
+  m_lpf_freq = new juce::AudioParameterFloat(
+      juce::ParameterID("lpf_freq", 1), "Low Cutoff",
+      juce::NormalisableRange(20.0f, 20000.0f), 5.0f);
+  addParameter(m_lpf_freq);
+  m_lpf_res = new juce::AudioParameterFloat(
+      juce::ParameterID("lpf_res", 1), "Resonance",
+      juce::NormalisableRange(0.1f, 100.0f), 0.71f);
+  addParameter(m_lpf_res);
+}
 
 GlynthProcessor::~GlynthProcessor() {}
 
@@ -33,9 +51,13 @@ bool GlynthProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const {
 void GlynthProcessor::processBlock(juce::AudioBuffer<float>& buffer,
                                    juce::MidiBuffer& midi_messages) {
   juce::ScopedNoDenormals noDenormals;
-  for (auto i = 0; i < getTotalNumOutputChannels(); i++) {
+  for (int ch = 0; ch < getTotalNumOutputChannels(); ch++) {
+    for (int i = 0; i < buffer.getNumSamples(); i++) {
+      auto rand_value = m_dist(m_gen);
+      buffer.setSample(ch, i, rand_value);
+    }
     // Clear unused output buffers to avoid garbage data blasting speakers
-    buffer.clear(i, 0, buffer.getNumSamples());
+    // buffer.clear(ch, 0, buffer.getNumSamples());
   }
 
   for (auto&& msg_metadata : midi_messages) {
@@ -44,12 +66,16 @@ void GlynthProcessor::processBlock(juce::AudioBuffer<float>& buffer,
     auto&& description = msg.getDescription().toStdString();
     fmt::println("MIDI message at buffer sample {}: {}", sample, description);
   }
+
+  // Check output for earrape and silence buffer if present
+  m_silencer.processBlock(buffer);
 }
 
 //==============================================================================
 
 juce::AudioProcessorEditor* GlynthProcessor::createEditor() {
-  return new GlynthEditor(*this);
+  return new juce::GenericAudioProcessorEditor(*this);
+  // return new GlynthEditor(*this);
 }
 
 //==============================================================================
@@ -68,4 +94,39 @@ void GlynthProcessor::setStateInformation(const void* data, int size) {
 
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter() {
   return new GlynthProcessor();
+}
+
+void CorruptionSilencer::processBlock(juce::AudioBuffer<float>& buffer) {
+  bool warned = false;
+  for (int ch = 0; ch < buffer.getNumChannels(); ch++) {
+    auto samples = buffer.getWritePointer(ch);
+    for (int i = 0; i < buffer.getNumSamples(); i++) {
+      float x = samples[i];
+      bool should_silence = false;
+      if (std::isnan(x) || std::isinf(x)) {
+        should_silence = true;
+        if (!warned) {
+          fmt::println(stderr, "Warning: audio buffer contains inf or nan");
+          warned = true;
+        }
+      } else if (std::abs(x) > 2.0f) {
+        should_silence = true;
+        if (!warned) {
+          fmt::println(stderr, "Warning: sample significantly out of range");
+          warned = true;
+        }
+      } else if (std::abs(x) > 1.0f) {
+        samples[i] = std::clamp(x, -1.0f, 1.0f);
+        if (!warned) {
+          fmt::println(stderr, "Warning: clamped out of range sample");
+          warned = true;
+        }
+      }
+
+      if (should_silence) {
+        buffer.clear(ch, 0, buffer.getNumSamples());
+        return;
+      }
+    }
+  }
 }
--- src/processor.h
+++ src/processor.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
+#include <random>
+
+class CorruptionSilencer {
+public:
+  void processBlock(juce::AudioBuffer<float>& buffer);
+};
 
 //==============================================================================
 class GlynthProcessor final : public juce::AudioProcessor {
@@ -45,6 +51,16 @@ private:
   //==============================================================================
   inline static auto s_io_layouts = BusesProperties().withOutput(
       "Output", juce::AudioChannelSet::stereo(), true);
+
+  juce::AudioParameterFloat* m_hpf_freq;
+  juce::AudioParameterFloat* m_hpf_res;
+  juce::AudioParameterFloat* m_lpf_freq;
+  juce::AudioParameterFloat* m_lpf_res;
+
+  CorruptionSilencer m_silencer;
+  std::random_device m_rd;
+  std::mt19937 m_gen;
+  std::uniform_real_distribution<float> m_dist;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GlynthProcessor)
 };
